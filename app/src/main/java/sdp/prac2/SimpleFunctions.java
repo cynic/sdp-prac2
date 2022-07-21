02572c2b9c267cd2449862d4cb6c6c0fff43846d
Ronell3281 <g18s9830@campus.ru.ac.za>
2022-07-21 15:30:57 +0200

    Task1 update

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -4,4 +4,16 @@ import java.util.*;
 
 public class SimpleFunctions {
     public SimpleFunctions() {}
+    public static int Task1(List<Integer> a, List<Integer> b) {
+
+        int sum = 0;
+        for (int i = 0; i < b.size(); i++) {
+            if (b.get(i) < (a.size() - 1)) {
+                sum = sum + a.get(b.get(i));
+            }
+
+        }
+        return sum;
+
+    }
 }
