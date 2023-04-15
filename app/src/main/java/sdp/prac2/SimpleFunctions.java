6b160d52c4f92ab2f299712fe7779aa252fe617c
Panashe Matirekwe <matirekwe3@gmail.com>
2023-04-15 19:09:28 +0200

    Implemented Task4 method

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -3,5 +3,17 @@ package sdp.prac2;
 import java.util.*;
 
 public class SimpleFunctions {
+    public static List<Integer> Task4(List<Integer> a, List<Integer> b) {
+    if (a.size() != b.size()) {
+        return null;
+    }
+    List<Integer> result = new ArrayList<Integer>();
+    int size = a.size();
+    for (int i = 0; i < size; i++) {
+        result.add(a.get(i) * b.get(size - i - 1));
+    }
+    return result;
+}
+
     public SimpleFunctions() {}
 }
