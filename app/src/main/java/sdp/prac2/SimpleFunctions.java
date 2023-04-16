d42f3164e59b81577b0e5aa4a9f0e55b3baa1a34
lonwabomfazwe <g21m4638@campus.ru.ac.za>
2023-04-16 19:37:05 +0200

    Task2 method

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -4,4 +4,17 @@ import java.util.*;
 
 public class SimpleFunctions {
     public SimpleFunctions() {}
+
+    public List<String> Task2(List<String> s) {
+        List<String> result = new ArrayList<String>();
+        for (String a : s) {
+            if (a.length() > 0) {
+                result.add(a.substring(1, a.length()));
+            }
+            else {
+                continue;
+            }
+        }
+        return result;
+    }
 }
