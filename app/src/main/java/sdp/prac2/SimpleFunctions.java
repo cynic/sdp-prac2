7d039b3e0477d2f6e1c818bff57e21782a978b18
Roland <rolandchumatc@gmail.com>
2023-04-16 17:04:47 +0200

    Added task 1

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -4,4 +4,15 @@ import java.util.*;
 
 public class SimpleFunctions {
     public SimpleFunctions() {}
+
+    public static int task1(List<Integer> a, List<Integer> b) {
+        int sum = 0;
+        for (int i = 0; i < b.size(); i++) {
+            int index = b.get(i);
+            if (index >= 0 && index < a.size()) {
+                sum += a.get(index);
+            }
+        }
+        return sum;
+    }
 }
