056bd6c83453b8ce987318deb97377174d081e8c
Sicelo Mduna <g21M7267@ict.ru.ac.za>
2023-04-18 00:27:08 +0200

    Completed the method Task 4

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -16,6 +16,42 @@ public class SimpleFunctions {
         return sum;
     }
 
+                                        //task 4
+/**
+
+This method takes in two lists of integers, "a" and "b". If the two input lists are of different sizes,
+
+the method returns null. Otherwise, it multiplies the first element of "a" with the last element of "b",
+
+the second element of "a" with the second-last element of "b", and so on. The resulting products are stored
+
+in a new list and returned.
+
+@param a the first input list of integers
+
+@param b the second input list of integers
+
+@return a list of integers, where each element is the product of a corresponding element from list "a"
+
+and list "b", or null if the two input lists are of different sizes
+*/
+    public static List<Integer> task4(List<Integer> a, List<Integer> b) {
+        if (a.size() != b.size()) {
+            return null; // Lists must be the same size to perform the task
+        }
+    
+        List<Integer> result = new ArrayList<>();
+        int lastIndex = b.size() - 1;
+    
+        for (int i = 0; i < a.size(); i++) {
+            int elementA = a.get(i);
+            int elementB = b.get(lastIndex - i);
+            result.add(elementA * elementB);
+        }
+    
+        return result;
+    }
+
     //task 5, returning true if the list is sorted (method for sorting integers)
     public static boolean task5(List<Integer> list) {
         if (list == null || list.isEmpty()) {
