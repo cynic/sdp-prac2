9df57867909128acde3671d1d144643f796581b3
vanSandwyk <van.sandwyk43@gmail.com>
2023-04-17 08:24:39 +0200

    Added a method called Task 1 for List Adding in SimpleFunctions.java

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -4,4 +4,14 @@ import java.util.*;
 
 public class SimpleFunctions {
     public SimpleFunctions() {}
+
+    public static int Task1(int[] a, int[] b) {
+    int total = 0; // Initialize accumulator
+    for (int index : b) { // Loop through each index in the array b
+        if (index >= 0 && index < a.length) { // Check if the index is within range of the array a
+            total += a[index]; // Add the corresponding element from a to the total
+        }
+    }
+    return total; // Return the total sum
+    }
 }
