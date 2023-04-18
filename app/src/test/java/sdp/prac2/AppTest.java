d50e3f513db1a9450ca9a44e6a344c1eb17ae7dd
naledikhumalo <nalediletseka@gmail.com>
2023-04-18 11:56:01 +0200

    TestTask2 added

--- app/src/test/java/sdp/prac2/AppTest.java
+++ app/src/test/java/sdp/prac2/AppTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
-    //tests for task1
+    tests for task1
     @Test void task1Test(){
         //Arrange 
         SimpleFunctions testTested = new SimpleFunctions();
@@ -15,7 +15,8 @@ class AppTest {
         List<Integer> b = Arrays.asList(0,1,2,3,4,5,6,7);
         List<Integer> c = Arrays.asList(1,2,3,4);
         List<Integer> d = Arrays.asList(5,1,7,2);
-        int expected1 = 11;
+    
+        int expected1 = 11; // isnt this meant to be 10? (1 + 2 + 3 + 4)
         int expected2 = 5;
         //Act
         int result1 = testTested.task1(a,b);
@@ -25,13 +26,16 @@ class AppTest {
         Assert.assertEquals(expected2,result2);
     }
 
-    //TASK 2 tests
+    TASK 2 tests
     @Test
     public void testTask2() {
+
+        SimpleFunctions Tests = new SimpleFunctions();
+
         List<String> input = Arrays.asList("hello", "world", "a", "", "testing", "123", "ab");
         List<String> expected = Arrays.asList("ello", "orld", "", "esting", "23", "b");
 
-        List<String> result = Task2(input);
+        List<String> result = Tests.Task2(input);
 
         assertEquals(expected, result);
     }//TASK 2 tests
